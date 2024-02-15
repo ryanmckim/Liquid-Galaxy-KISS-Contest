5762e18b372596bc177da5063287a88bfe717464
ryanmckim <mck649@gmail.com>
2024-02-15 15:49:10 -0800

    Done!

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,230 @@
 import 'package:flutter/material.dart';
+import 'package:dartssh2/dartssh2.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      navigatorKey: navigatorKey,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple Flutter App'),
+        ),
+        body: Center(
+          child: Image.asset('assets/logo.png'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.restart_alt),
+              label: 'Reboot',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home City',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.circle),
+              label: 'Orbit',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: 'Image',
             ),
           ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue[900],
+          unselectedItemColor: Colors.grey,
+          onTap: _onItemTapped,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        rebootLG();
+        break;
+      case 1:
+        moveToHomeCity();
+        break;
+      case 2:
+        makeOrbit();
+        break;
+      case 3:
+        displayImage();
+        break;
+    }
+  }
+
+  void rebootLG() async {
+    final bool? shouldReboot = await showDialog<bool>(
+      context: navigatorKey.currentContext!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Restart confirmation!'),
+          content: const Text('Do you want to reboot the LG?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (shouldReboot == true) {
+      SSHClient client = SSHClient(
+        await SSHSocket.connect("192.168.56.102", 22),
+        username: "lg",
+        onPasswordRequest: () => "lg",
+      );
+
+      for (var i = 1; i <= 3; i++) {
+        final command = """
+          SERVICE=\$(if [ -f /etc/init/lxdm.conf ]; then echo lxdm; elif [ -f /etc/init/lightdm.conf ]; then echo lightdm; fi)
+          if [ -z "\$SERVICE" ]; then exit 1; fi
+          STATUS=\$(service \$SERVICE status)
+          if [[ "\$STATUS" =~ 'stop' ]]; then
+            echo lg | sudo -S service \$SERVICE start
+          else
+            echo lg | sudo -S service \$SERVICE restart
+          fi
+          sshpass -p lg ssh -x -t lg@lg$i "echo lg | sudo -S service \$SERVICE restart"
+        """;
+
+        await client.execute("/home/lg/bin/lg-relaunch");
+        await client.execute(command);
+      }
+    }
+  }
+
+  void moveToHomeCity() async {
+    SSHClient client = SSHClient(
+      await SSHSocket.connect("192.168.56.102", 22),
+      username: "lg",
+      onPasswordRequest: () => "lg",
+    );
+
+    String vancouverKML = '''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">
+        <Document>
+          <LookAt>
+            <longitude>-123.1207</longitude>
+            <latitude>49.2827</latitude>
+            <altitude>0</altitude>
+            <range>10000</range>
+            <tilt>0</tilt>
+            <heading>0</heading>
+            <gx:altitudeMode>relativeToGround</gx:altitudeMode>
+          </LookAt>
+        </Document>
+      </kml>
+    ''';
+
+    await client.execute('> /var/www/html/kmls.txt');
+    await client
+        .execute("echo '''$vancouverKML''' > /var/www/html/kml/vancouver.kml");
+    await client.execute(
+        'echo "http://lg1:81/kml/vancouver.kml" > /var/www/html/kmls.txt');
+    await client.execute(
+        'echo "flytoview=<LookAt><longitude>-123.1207</longitude><latitude>49.2827</latitude><altitude>0</altitude><range>10000</range><tilt>0</tilt><heading>0</heading><gx:altitudeMode>relativeToGround</gx:altitudeMode></LookAt>" > /tmp/query.txt');
+  }
+
+  void makeOrbit() async {
+    SSHClient client = SSHClient(
+      await SSHSocket.connect("192.168.56.102", 22),
+      username: "lg",
+      onPasswordRequest: () => "lg",
+    );
+
+    String generateOrbitKML() {
+      double startLongitude = -123.1207;
+      double startLatitude = 49.2827;
+
+      double endLongitude = -122.3321;
+      double endLatitude = 47.6062;
+
+      double longitudeIncrement = (endLongitude - startLongitude) / 36;
+      double latitudeIncrement = (endLatitude - startLatitude) / 36;
+
+      String flyToElements = List.generate(36, (index) {
+        double currentLongitude = startLongitude + (index * longitudeIncrement);
+        double currentLatitude = startLatitude + (index * latitudeIncrement);
+        return '''
+        <gx:FlyTo>
+          <gx:duration>10</gx:duration>
+          <gx:flyToMode>smooth</gx:flyToMode>
+          <LookAt>
+            <longitude>$currentLongitude</longitude>
+            <latitude>$currentLatitude</latitude>
+            <altitude>5000</altitude>
+            <heading>0</heading>
+            <tilt>45</tilt>
+            <range>5000</range>
+            <gx:altitudeMode>relativeToGround</gx:altitudeMode>
+          </LookAt>
+        </gx:FlyTo>
+      ''';
+      }).join("\n");
+
+      return '''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">
+      <Document>
+        <name>From Vancouver Tour</name>
+        <gx:Tour>
+          <name>Travel Tour</name>
+          <gx:Playlist>
+            $flyToElements
+          </gx:Playlist>
+        </gx:Tour>
+      </Document>
+      </kml>
+    ''';
+    }
+
+    String orbitKML = generateOrbitKML();
+
+    await client.execute('> /var/www/html/kmls.txt');
+    await client.execute("echo '''$orbitKML''' > /var/www/html/TravelTour.kml");
+    await client.execute(
+        "echo 'http://lg1:81/TravelTour.kml' >> /var/www/html/kmls.txt");
+    await client.execute('echo "playtour=Travel Tour" > /tmp/query.txt');
+  }
+
+  Future<void> displayImage() async {
+    SSHClient client = SSHClient(
+      await SSHSocket.connect("192.168.56.102", 22),
+      username: "lg",
+      onPasswordRequest: () => "lg",
+    );
+
+    await client.execute(
+        "sshpass -p lg ssh -o StrictHostKeyChecking=no -t lg@lg2 'DISPLAY=:0 feh --geometry 400x400 \"/home/lg/Downloads/name.png\" --borderless'");
   }
 }
